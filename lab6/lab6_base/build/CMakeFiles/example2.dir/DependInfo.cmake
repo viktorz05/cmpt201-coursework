b7d4404b4924384916af1e417b2768c9b32e12d4
Viktor Zepeda <vza12@sfu.ca>
2026-03-01 23:28:03 -0800

    lab6

--- lab6/lab6_base/build/CMakeFiles/example2.dir/DependInfo.cmake
+++ lab6/lab6_base/build/CMakeFiles/example2.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/viktorzepeda/codesfu/cmpt201-coursework/lab6/lab6_base/example_2.c" "CMakeFiles/example2.dir/example_2.c.o" "gcc" "CMakeFiles/example2.dir/example_2.c.o.d"
+  "/home/cmpt201/cmpt201-coursework/lab6/lab6_base/example_2.c" "CMakeFiles/example2.dir/example_2.c.o" "gcc" "CMakeFiles/example2.dir/example_2.c.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
